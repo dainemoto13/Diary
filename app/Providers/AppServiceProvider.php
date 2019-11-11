9d95d92c5f6b129cc8637aa092369c5c0ac68a49
根本大 <>
2019-11-11 11:34:01 +0800

    データベース接続設定完了

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; //追加
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //追加
+        Schema::defaultStringLength(191);
     }
 
     /**
